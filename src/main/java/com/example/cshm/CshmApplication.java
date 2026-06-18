cf38cae93912f70d4dfc2ffaac3e92835c90980e
sk8.quist <2116331610@qq.com>
2026-06-18 12:43:26 +0800

    feat: 初始化校园二手交易平台项目

---
+++ src/main/java/com/example/cshm/CshmApplication.java
@@ -0,0 +1,13 @@
+package com.example.cshm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CshmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CshmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cshm/CshmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cshm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CshmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
